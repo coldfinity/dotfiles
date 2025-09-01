3363bf643d04e2bced1441cc063462cc7b6e6da8
coldfinity <wuyd0815@gmail.com>
2025-09-01 11:56:24 +1000

    aerospace added to sketchybar

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-for sid in $(aerospace list-workspaces --all); do
+sketchybar --add event aerospace_workspace_change
+
+letters=("1" "2" "3" "4" "5")
+
+for sid in "${letters[@]}"; do
   sketchybar --add item space.$sid left \
     --subscribe space.$sid aerospace_workspace_change \
     --set space.$sid \
-    background.color=0x44ffffff \
-    background.corner_radius=5 \
-    background.height=20 \
-    background.drawing=off \
+    label.padding_left=0 \
+    label.padding_right=13 \
     label="$sid" \
     click_script="aerospace workspace $sid" \
-    script="$PLUGIN_DIR/aerospace.sh $sid"
+    script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
