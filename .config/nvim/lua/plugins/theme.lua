c52c205ce39120f554254354757e237771ee3e87
coldfinity <wuyd0815@gmail.com>
2025-09-30 01:22:30 +1000

    Updated

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -22,7 +22,7 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local transparent = true
+      local transparent = false
       require("github-theme").setup({
         options = {
           transparent = transparent,
