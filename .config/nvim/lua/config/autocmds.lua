7274c4ee09d50c0b1561f4a6e1a1d456ffab6fa3
Yudi Wu <wuyd0815@gmail.com>
2026-05-04 16:43:44 +1000

    1

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -18,3 +18,10 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 ]]
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "rmarkdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
